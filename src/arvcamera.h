d68edb06b329a460f34d3536d09a8dbcd28185a2
Mitesh Dube <49096865+miteshdube@users.noreply.github.com>
2021-12-14 17:21:44 +0530

    Update arvcamera.h

--- src/arvcamera.h
+++ src/arvcamera.h
@@ -23,7 +23,7 @@
 #ifndef ARV_CAMERA_H
 #define ARV_CAMERA_H
 
-#if !defined (ARV_H_INSIDE) && !defined (ARAVIS_COMPILATION)
+#if !defined (ARV_H_INSIDE) && !defined (ARAVIS_COMPILATION) && !defined(EXTENSION_BUILD)
 #error "Only <arv.h> can be included directly."
 #endif
 
