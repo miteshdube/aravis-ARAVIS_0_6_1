844a421ae922ba87beb115d6830695bfe7418a45
Mitesh Dube <49096865+miteshdube@users.noreply.github.com>
2021-12-14 17:26:59 +0530

    Update arvtypes.h

--- src/arvtypes.h
+++ src/arvtypes.h
@@ -23,7 +23,7 @@
 #ifndef ARV_TYPES_H
 #define ARV_TYPES_H
 
-#if !defined (ARV_H_INSIDE) && !defined (ARAVIS_COMPILATION)
+#if !defined (ARV_H_INSIDE) && !defined (ARAVIS_COMPILATION) && !defined(EXTENSION_BUILD)
 #error "Only <arv.h> can be included directly."
 #endif
 
